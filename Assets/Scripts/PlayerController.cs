c181b0edf0cb8de29ea446c7108cf56dc871cb2b
Th3LastCodeBender <parolisi.leonardo@gmail.com>
2026-01-31 16:42:55 +0100

    First push

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,24 +1,54 @@
+using UnityEngine.InputSystem;
 using UnityEngine;
 using System;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Movimento")]
+    public float moveSpeed = 10f;
+    public float jumpForce = 10f;
+
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float groundDistance = 0.1f;
+    public LayerMask groundMask;
+
+    private Rigidbody rb;
+    private bool isGrounded;
+
     void Start()
     {
-     
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Ground Check
+        isGrounded = Physics.Raycast(groundCheck.position, Vector3.down, groundDistance, groundMask);
     }
 
-    public void Move() {
-      Debug.Log("Player is moving");
+	private void MovePad(InputAction.CallbackContext context)
+	{
+		Vector2 moveInput = context.ReadValue<Vector2>(); // -1 o 1
+        Vector3 velocity = rb.linearVelocity;
+        moveInput.x = moveInput.x * moveSpeed;
+		moveInput.y = isGrounded ? moveInput.y : moveInput.y * moveSpeed;
+        rb.linearVelocity = velocity;
+		Debug.Log("Player is moving!");
+	}
+    public void Move(InputAction.CallbackContext context){
+		Vector2 moveInput = context.ReadValue<Keyboard>(); // -1 o 1
+        Vector3 velocity = rb.linearVelocity;
+        moveInput.x = moveInput.x * moveSpeed;
+		moveInput.y = isGrounded ? moveInput.y : moveInput.y * moveSpeed;
+        rb.linearVelocity = velocity;
+		Debug.Log("Player is moving!");
     }
     public void Jump() {
+		if (isGrounded)
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
+        }
       Debug.Log("Player is jumping");
     }
     public void ThrowMask() {
