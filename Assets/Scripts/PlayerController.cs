93516e1a93e1f5a572e2ede4c9a5b3294501eccc
fiordarancio <tono.ilaria@gmail.com>
2026-01-31 18:21:38 +0100

    feat: movement correct

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,48 +1,76 @@
 using UnityEngine;
 using System;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+  [Header("Movement")]
+  public float moveSpeed = 10f;
+  public float jumpForce = 10f;
+
+  [Header("Ground Check")]
+  public Transform groundCheck;
+  public float groundDistance = 3.0f;
+  public LayerMask groundMask;
+
+  private Rigidbody rb;
+  private bool isGrounded = false;
+
+  // Start is called once before the first execution of Update after the MonoBehaviour is created
+  void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    // Ground Check
+    isGrounded = Physics.Raycast(groundCheck.position, Vector3.down, groundDistance, groundMask);
+  }
+
+  public void Move(InputAction.CallbackContext context)
+  {
+    Vector2 moveInput = context.ReadValue<Vector2>();
+    Vector3 currentVelocity = rb.linearVelocity;
+    currentVelocity.x = moveInput.x * moveSpeed;
+    rb.linearVelocity = currentVelocity;
+  }
+  
+  public void Jump()
+  {
+    Debug.Log("Player is jumping");
+  }
+
+  public void ThrowMask()
+  {
+    Debug.Log("Player is throwing a mask");
+    Transform maskTransform = transform.Find("Mask1");
+
+    if (maskTransform != null)
     {
-     
-    }
+      MaskScript mask = maskTransform.gameObject.GetComponent<MaskScript>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void Move() {
-      Debug.Log("Player is moving");
-    }
-    public void Jump() {
-      Debug.Log("Player is jumping");
-    }
-    public void ThrowMask() {
-      Debug.Log("Player is throwing a mask");
-      Transform maskTransform = transform.Find("Mask1");
-
-      if (maskTransform != null)
+      if (mask.CanLaunch())
       {
-        MaskScript mask = maskTransform.gameObject.GetComponent<MaskScript>();
-
-        if (mask.CanLaunch())
-        {
-          mask.LaunchMask();
-        }
+        mask.LaunchMask();
       }
     }
-    public void Dash() {
-      Debug.Log("Player is dashing");
-    }
-    public void PauseGame() {
-      Debug.Log("Game is paused");
-    }
-    public void Parry() {
-      Debug.Log("Player is parrying");
-    }
+  }
+
+  public void Dash()
+  {
+    Debug.Log("Player is dashing");
+  }
+
+  public void PauseGame()
+  {
+    Debug.Log("Game is paused");
+  }
+  
+  public void Parry()
+  {
+    Debug.Log("Player is parrying");
+  }
 
 }
